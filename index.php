247c71f2543ec2a453e88c659b79eda480ae4046
Jarrar Jaffari <jjaffari@cisco.com>
2016-11-14 07:42:32 -0500

    made date base difference work

--- index.php
+++ index.php
@@ -3,23 +3,21 @@ include 'namazTimes.php';
 date_default_timezone_set('America/New_York');
 
 $namaz = new NamazTime();
-$data = $namaz -> get_namaz_times_for_today();
-$today = date("F j, Y, g:i a");
+$namaz_times = $namaz -> get_namaz_times_for_today();
+$today_date = date("M/d/y g:i a");
 
-echo "Today's date: $today ";
-
-if ($namaz -> is_daylight_savings()) {
-	echo "DST <br>";
-}
+// if ($namaz -> is_daylight_savings()) {
+	// echo "DST <br>";
+// }
 
 // foreach ($data as $key => $value) {
 // # conversion into 12 Hr time
 // $namaz_time = date('g:i a', strtotime($value));
 // echo "{$key} : {$namaz_time} <br>";
 // }
-?>
+//?>
 
-<table align="left">
+<!--table align="left">
 	<tr>
 		<td>
 		<table id="namazTable" cellSpacing="0" cellPadding="0" width="500" border="0">
@@ -40,4 +38,81 @@ if ($namaz -> is_daylight_savings()) {
 				?>
 			</tr>
 		</table>
-</table>
+</table-->
+<style type="text/css">
+	body {
+		background-color: #F8F8F8;
+	}
+
+	td {
+		color: #3D3D3D;
+		font-family: tahoma;
+		font-size: 12px;
+	}
+
+	.top {
+		font-family: tahoma;
+		color: #3D3D3D;
+		font-size: 11px;
+		font-weight: bold;
+		text-decoration: none;
+	}
+	.top:hover {
+		font-family: tahoma;
+		color: #3D3D3D;
+		font-size: 11px;
+		font-weight: bold;
+		text-decoration: underline;
+	}
+
+	table.namaz, th.namaz, td.namaz, tr.namaz {
+		border: 0px solid black;
+		border-collapse: collapse;
+		font-family: tahoma;
+		font-size: 12px;
+	}
+
+</style>
+
+<!--?php print date('H'); ?-->
+<table class="namaz" style="padding:0px">
+	<tr><td align="left" class="namaz" width="150"><?php echo $today_date?></td></tr>
+	<tr><td align="center" class="namaz">
+	<table align="left" class="namaz" style="padding:0px">
+		<?php
+
+		// WILL use this mechanism to find if the date is different from when last we picked the times.
+		$current = strtotime(date("Y-m-d"));
+		$date = strtotime("2016-11-12");
+
+		$datediff = $date - $current;
+		$difference = floor($datediff / (60 * 60 * 24));
+		
+		print "<br>";
+		if ($difference == 0) {
+			print "$date : today";
+		} else if ($difference > 1) {
+			print '$date : Future Date';
+		} else if ($difference > 0) {
+			print '$date : tomarrow';
+		} else if ($difference < -1) {
+			print '$date : Long Back';
+		} else {
+			print '$date : yesterday';
+		}
+
+		foreach ($namaz_times as $key => $value) {
+
+			echo "<TR align=\"right\" class=\"namaz\">";
+			echo "<td class=\"namaz\">{$key}:</td>";
+			// convert it into 12 Hour time format
+			$namaz_time = date('g:i', strtotime($value));
+			echo "<td class=\"namaz\" align=\"right\">{$namaz_time}</td>";
+			echo "</TR>";
+		}
+		?>
+		</table>
+		</tr>
+	</td>
+	</tr>
+</table
--- namazTimes.php
+++ namazTimes.php
@@ -1,53 +1,118 @@
 <?php
 class NamazTime {
-    private $year = '';
-    private $month = '';
-    private $day = '';
+	private $year = '';
+	private $month = '';
+	private $day = '';
 
-    const PRAYTIME_URL = "http://praytime.info/getprayertimes.php";
-    const IABAT_LAT = "35.839742";
-    const IABAT_LONG = "-78.893319";
-    const GMT = "-240";
+	private $org_lat = '';
+	private $org_long = '';
 
-    function __construct() {
-        $this -> year = date("Y");
-        $this -> month = date("n");
+	const PRAYTIME_URL = "http://praytime.info/getprayertimes.php";
+	// In DST -240 and when it is EST then -300
+	const GMT_DST = "-240";
+	const GMT_EST = "-300";
 
-        $today = date("F j, Y, g:i a");
-        $day = date("j");
+	function __construct() {
+		$this -> year = date("Y");
+		$this -> month = date("n");
+		$this -> org_lat = '';
+		$this -> org_long = '';
 
-    }
+		$today = date("F j, Y, g:i a");
+		$day = date("j");
 
-    public function is_daylight_savings() {
-        return date('I') == 1;
-    }
+		$this -> determin_coordinates();
+	}
 
-    private function namaz_times($day = "all") {
-        $days_option = "";
-        if ($day != "all") {
-            $days_option = "&d=$day";
-        }
+	// wanna get the address from
+	private function make_geo_location_url() {
+		return "http://maps.googleapis.com/maps/api/geocode/json?address=4130+Plum+Branch+dr,+cary,+NC,+27519&sensor=false";
+	}
 
-        $iabat_long = self::IABAT_LONG;
-        $ibat_lat = self::IABAT_LAT;
-        $gmt = self::GMT;
+	private function latitude($data) {
+		return $data['results'][0]["geometry"]["location"]["lat"];
+	}
 
-        $url = self::PRAYTIME_URL . "?lat=$iabat_lat&lon=$iabat_long&gmt=$gmt&m=$this->month&y=$this->year&school=0$days_option";
+	private function longitude($data) {
+		return $data['results'][0]["geometry"]["location"]["lng"];
+	}
 
-        $json = file_get_contents($url);
-        $data = json_decode($json, true);
-        return $data;
-    }
+	private function get_coordinates_from_file() {
+		$str = file_get_contents('location.json');
+		$json = json_decode($str, true);
 
-    public function get_namaz_times_for_today() {
-        $day = date("j");
-        return $this -> namaz_times($day);
-    }
+		$this -> org_lat = $json['latitude'];
+		$this -> org_long = $json['longitude'];
+	}
 
-    // this is not tested yet - Jarrar
-    public function get_namaz_times_for_month() {
-        return $this -> namaz_times();
-    }
+	private function get_coordinates_from_google() {
+		$url = $this->make_geo_location_url();
+
+		$json = file_get_contents($url);
+		$data = json_decode($json, true);
+
+		$this -> org_lat = $this -> latitude($data);
+		$this -> org_long = $this -> longitude($data);
+
+		$org_location = array("zip_code" => "27519", "latitude" => "$this->org_lat", "longitude" => "$this->org_long");
+		$fp = fopen('location.json', 'w');
+		fwrite($fp, json_encode($org_location));
+		fclose($fp);
+	}
+
+	private function determin_coordinates() {
+		if (file_exists('location.json')) {
+			$this -> get_coordinates_from_file();
+			return;
+		}
+
+		$this -> get_coordinates_from_google();
+	}
+
+	private function is_daylight_savings() {
+		return date('I') == 1;
+	}
+
+	private function get_gmt_value() {
+		$my_gmt = self::GMT_EST;
+
+		if (self::is_daylight_savings()) {
+			$my_gmt = self::GMT_DST;
+		}
+
+		return $my_gmt;
+	}
+
+	private function namaz_times($day = "all") {
+		$days_option = "";
+		if ($day != "all") {
+			$days_option = "&d=$day";
+		}
+
+		$gmt = self::get_gmt_value();
+
+		$url = self::PRAYTIME_URL . "?lat=$this->org_lat&lon=$this->org_long&gmt=$gmt&m=$this->month&y=$this->year&school=0$days_option";
+
+		//print $url;
+		$json = file_get_contents($url);
+		$data = json_decode($json, true);
+		return $data;
+	}
+
+	public function get_namaz_times_for_today() {
+		$day = date("j");
+		return $this -> namaz_times($day);
+	}
+
+	// this is not tested yet - Jarrar
+	public function get_namaz_times_for_month() {
+		return $this -> namaz_times();
+	}
+
+	public function next_namaz() {
+		$namaz_times_24hr = $this -> get_namaz_times_for_today();
+		$today_date = date("M/j/y g:i a");
+	}
 
 }
 ?>
